1f7b83a639f12980739c70531d53329289f11999
elvece <lvcifferello@gmail.com>
2015-07-29 16:51:00 -0600

    reveresed sections that were commented out; other exercises will need to be commented out to make each exercise work

--- boxes.js
+++ boxes.js
@@ -12,45 +12,45 @@ $(function() {
 });
 
 //exercise 2
-// $(function() {
+$(function() {
 
-//   $("#secretBox").css("background", "white");
+  $("#secretBox").css("background", "white");
 
-//   $("#secretBox").html("<h1>Secret Box!</h1>").css("color", "black");
+  $("#secretBox").html("<h1>Secret Box!</h1>").css("color", "black");
 
-//   $("#row1").children().attr("class", "box boxType3");
+  $("#row1").children().attr("class", "box boxType3");
 
-//   $("#row4 div:last-child").css("display", "none");
+  $("#row4 div:last-child").css("display", "none");
 
-//   $(".boxType1").css("background", "white");
+  $(".boxType1").css("background", "white");
 
-//   $(".box").not("#secretBox").css("width", "2px");
+  $(".box").not("#secretBox").css("width", "2px");
 
-// });
+});
 
 
 // //exercise 3
 $(function(){
 
-  // $("#container").on("click", function(event) {
-  //   console.log(event.pageX, event.pageY);
-  // });
+  $("#container").on("click", function(event) {
+    console.log(event.pageX, event.pageY);
+  });
 
-  // $(".boxType1").append('<a href="https://www.google.com">Galvanize</a>');
+  $(".boxType1").append('<a href="https://www.google.com">Galvanize</a>');
 
-  // $(".boxType1 a" ).on("click", function(event){
-  //   event.preventDefault();
-  //   alert("You cannot leave the page!");
-  // });
+  $(".boxType1 a" ).on("click", function(event){
+    event.preventDefault();
+    alert("You cannot leave the page!");
+  });
 
-  // $(".box").on("click", function(){
-  //   if ($(this).has('img').length) {
-  //     $(this).find('img').remove();
-  //   }
-  //   else {
-  //     $(this).append('<img src="http://puppydogweb.com/gallery/puppies/labradorretriever2.jpg" alt="cute puppy" width="100%">');
-  //   }
-  // });
+  $(".box").on("click", function(){
+    if ($(this).has('img').length) {
+      $(this).find('img').remove();
+    }
+    else {
+      $(this).append('<img src="http://puppydogweb.com/gallery/puppies/labradorretriever2.jpg" alt="cute puppy" width="100%">');
+    }
+  });
 
   var container = $('#container');
 
